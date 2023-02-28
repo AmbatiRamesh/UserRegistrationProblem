4af6a799557285c715670e549952841e360976cd
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-28 12:00:01 +0530

    Added: Enter a valid First Name

---
+++ UserRegistration.java
@@ -0,0 +1,8 @@
+package com.registration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        Validation validation = new Validation();
+        validation.checkFirstName();
+    }
+}
