d1c4be648a91fccded23e1b64d5f707140533577
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-02-28 12:32:20 +0530

    Added: Enter a valid Last Name

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,5 +4,6 @@ public class UserRegistration {
     public static void main(String[] args) {
         Validation validation = new Validation();
         validation.checkFirstName();
+        validation.checkLastName();
     }
 }
--- Validation.java
+++ Validation.java
@@ -2,14 +2,24 @@ package com.registration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 public class Validation {
-    Scanner sc=new Scanner(System.in);
-    public void checkFirstName(){
+    Scanner scanner = new Scanner(System.in);
+    public void checkFirstName() {
         System.out.println("Enter First Name : ");
-        String firstName=sc.next();
-        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}",firstName)){
+        String firstName = scanner.next();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}", firstName)) {
             System.out.println("First Name is Valid---!!");
-        }else {
+        } else {
             System.out.println("First Name is Invalid--!!");
+        }
+    }
+    public void checkLastName() {
+        System.out.println("-----------------");
+        System.out.println("Enter Last Name : ");
+        String lastName = scanner.next();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{3,}", lastName)) {
+            System.out.println("LastName is Valid---!!");
+        } else {
+            System.out.println("LastName is Invalid--!!");
         }
     }
 }
